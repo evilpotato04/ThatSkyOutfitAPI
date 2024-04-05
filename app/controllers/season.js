06c04a422ab54126baafbcec1198a2b591a80a8b
Samunta <samantalemos04@gmail.com>
2024-04-05 17:36:16 -0300

    [US02] T02 - Validate all fields inserted

--- app/controllers/season.js
+++ app/controllers/season.js
@@ -1,9 +1,42 @@
 module.exports.add = (app, req, res) => {
 
-    let jsonData = req.body;
+    validateData(app, req, res, valid => {
+        if (!valid) return
 
-    const connection = app.config.dbConnection;
-    const seasonDAO = new app.app.models.seasonDAO(connection);
+        const connection = app.config.dbConnection
+        const seasonDAO = new app.app.models.seasonDAO(connection)
 
-    seasonDAO.add(jsonData, req, res);
+        seasonDAO.add(req, res)
+    })
+}
+
+let validateData = (app, req, res, callback) => {
+    let errorMessage = null
+
+    if (errorMessage == null && typeof req.body.description != "string") errorMessage = "description field is invalid"
+    if (errorMessage == null && typeof req.body.startDate != "string") errorMessage = "startDate field is invalid"
+    if (errorMessage == null && typeof req.body.endDate != "string") errorMessage = "endDate field is invalid"
+
+    if (errorMessage == null && req.body.description == "") errorMessage = "description field can't be blank"
+    if (errorMessage == null && req.body.startDate == "") errorMessage = "startDate field can't be blank"
+    if (errorMessage == null && req.body.endDate == "") errorMessage = "endDate field can't be blank"
+    
+    validateDuplicity(app, req, res, valid => {
+        if (errorMessage == null && !valid) 
+            errorMessage = "This Season already exists in database"
+    
+        if (errorMessage != null)
+            res.status(406).json({ "status": 406, "error": errorMessage })
+        
+        errorMessage == null ? callback(true) : callback(false)
+    })
+}
+
+let validateDuplicity = (app, req, res, callback) => {
+    const connection = app.config.dbConnection
+    const seasonDAO = new app.app.models.seasonDAO(connection)
+
+    seasonDAO.get(req.body.description, null, null, res, (listSeasons) => {
+        listSeasons.length == 0 ? callback(true) : callback(false)
+    })
 }
--- app/models/seasonDAO.js
+++ app/models/seasonDAO.js
@@ -1,23 +1,50 @@
-function seasonDAO(connection){
-    this._connection = connection;
+function seasonDAO(connection) {
+    this._connection = connection
 }
 
-seasonDAO.prototype.add = function(json, req, res){
-    let sql = "INSERT INTO Season VALUES (?,?,?,?)"
-    let params = [json.idSeason, json.description, json.startDate, json.endDate]
+seasonDAO.prototype.add = function(req, res) {
+    let sql = "INSERT INTO Season (Name,StartDate,EndDate) VALUES (?,?,?)"
+    let params = [req.body.description, req.body.startDate, req.body.endDate]
 
     this._connection.run(sql, params, (err, result) => {
         if (err) {
-            res.status(400).json({ "error": err.message })
-            return;
+            res.status(500).json({ "status": 500, "error": "Internal Server Error: " + err.message })
+            return
         }
 
-        res.status(201).json({
-            "message": "Season registered successfully"
-        })
+        res.status(201).json({ "status": 201, "message": "Season registered successfully" })
+    })
+}
+
+seasonDAO.prototype.get = function(description, startDate, endDate, res, callback) {
+    let sql = "SELECT * FROM Season WHERE 1 = 1 "
+    let params = []
+    
+    if (description != null) {
+        sql += "AND Name = ? "
+        params.push(description)
+    }
+
+    if (startDate != null) {
+        sql += "StartDate = ? "
+        params.push(startDate)
+    }
+    
+    if (endDate != null) {
+        sql += "EndDate = ? "
+        params.push(endDate)
+    }
+
+    this._connection.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ "status": 500, "error": "Internal Server Error: " + err.message })
+            return
+        }
+
+        callback(rows)
     })
 }
 
 module.exports = () => {
-    return seasonDAO;
+    return seasonDAO
 }
